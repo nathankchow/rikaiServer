7797c6927f6fe9c083ac77196aad4b8550426332
Nathan Chow <nathankchow@ucsb.edu>
2022-07-17 09:57:17 +0900

    Added export to anki feature. \n\n Upon the socket event export_to_csv, a csv file that can be imported into anki is written at \documents\rikaiserver\anki_csv.

--- server.py
+++ server.py
@@ -6,6 +6,10 @@ import time
 eventlet.monkey_patch()
 import subprocess
 import socket
+import os 
+import ctypes.wintypes
+import uuid
+
 
 #get ip address
 def get_ip():
@@ -27,9 +31,11 @@ mgr = socketio.KombuManager("amqp://")
 write_only = socketio.KombuManager("amqp://", write_only=True)
 sio = socketio.Server(logger=True, engineio_logger=True, client_manager=mgr)
 
-app = socketio.WSGIApp(sio, static_files={
-    '/': {'content_type': 'text/html', 'filename': 'public/index.html'}
-})
+# app = socketio.WSGIApp(sio, static_files={
+#     '/': {'content_type': 'text/html', 'filename': 'public/index.html'}
+# })
+
+app = socketio.WSGIApp(sio)
 
 global emitting
 emitting = False
@@ -39,7 +45,7 @@ def connect(sid, environ):
     print('connect ', sid)
     global emitting
     if emitting==False:
-        sio.start_background_task(emitter) #make sure only one instance of emitter is running 
+        sio.start_background_task(txt_emitter) #make sure only one instance of emitter is running 
         emitting=True
     else:
         print("already emitting!")
@@ -47,23 +53,38 @@ def connect(sid, environ):
 @sio.event
 def my_message(sid, data):
     print('message ', data)
-    return "GOt your my message bro."
 
 @sio.event
 def disconnect(sid):
     print('disconnect ', sid)
 
-
 @sio.event
 def test(sid, data):
     print('test', data)
 
+@sio.event
+def export_to_csv(sid, data):
+    CSIDL_PERSONAL = 5       # My Documents
+    SHGFP_TYPE_CURRENT = 0   # Get current, not default value
+    buf= ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
+    ctypes.windll.shell32.SHGetFolderPathW(None, CSIDL_PERSONAL, None, SHGFP_TYPE_CURRENT, buf)
+    docu_path = buf.value
+    filename = str(uuid.uuid4())
+    filepath = os.path.join(docu_path, "rikaiServer", "anki_csv", filename + ".csv")
+    with open(filepath, mode='w+', encoding="utf-8") as f:
+        f.write(data)
+    if os.path.isfile(filepath):
+        print(f"CSV file written at {filepath}")
+        sio.emit("can_clear_review", "")
+    
+
+
 def listener(sio,app):
     print("Server started.")
     eventlet.wsgi.server(eventlet.listen((ip_address, port)), app)
-    print("Listener function unexpectedly terminated?")
 
-def emitter():
+
+def emitter(): #clipboard based 
     previous = pyperclip.paste()
     while True:
         current = pyperclip.paste()
@@ -78,7 +99,36 @@ def emitter():
         else:
             pass 
         eventlet.sleep(0.10)
- 
+
+def txt_emitter():
+    CSIDL_PERSONAL = 5       # My Documents
+    SHGFP_TYPE_CURRENT = 0   # Get current, not default value
+    buf= ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
+    ctypes.windll.shell32.SHGetFolderPathW(None, CSIDL_PERSONAL, None, SHGFP_TYPE_CURRENT, buf)
+    docu_path = buf.value
+    dir = os.path.join(docu_path, "rikaiServer", "temp")
+    for f in os.listdir(dir):
+        if f.endswith(".txt"):
+            os.remove(os.path.join(dir,f)) #remove all files upon initialization 
+    print("removed all files")
+    
+    while True:
+        files = os.listdir(dir)
+        if len(files) != 0:
+            for file in files:
+                if file.endswith(".txt"):
+                    file_path = os.path.join(dir,file)
+                    eventlet.sleep(0.01) #sleep 10 ms to avoid potential race condition 
+                    with open(file_path, 'r') as f:
+                        raw = f.read() 
+                    os.remove(file_path)
+                    sio.emit('message', {'data': raw})
+                    eventlet.sleep(0)
+                    p = Process(target=emit_info_from_kombu, args=(raw,))
+                    p.start()
+        eventlet.sleep(0.15) #poll directory every 150 ms 
+                    
+        
 
 def emit_info_from_kombu(msg):
     try:
@@ -97,6 +147,7 @@ def emit_info_from_kombu(msg):
         "raw" : msg,
         "info": info
     })
+
 
 def main():
     listener(sio,app)
